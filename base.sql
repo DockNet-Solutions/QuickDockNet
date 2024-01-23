171050d959be935f176870b560ed67e8400b20ea
Hugo MATHIEU <44479509+guedesite@users.noreply.github.com>
2024-01-23 17:24:06 +0100

    add recover sql table

--- base.sql
+++ base.sql
@@ -7,3 +7,12 @@ CREATE TABLE `users` (
   `active` int(1) DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `passRecover` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `token` varchar(32) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `createdAt` int(11) NOT NULL,
+    `userId` int(11) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
